519b720e2c2a6a28170a38bca0de5d38027b8bbe
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2022-11-21 15:00:17 +0530

    Add files via upload

---
+++ String_Buffer_insert_Method.java
@@ -0,0 +1,8 @@
+class String_Buffer_insert_Method
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb = new StringBuffer("java progrraming");
+		System.out.println(sb.insert(3,"m"));
+	}
+}
